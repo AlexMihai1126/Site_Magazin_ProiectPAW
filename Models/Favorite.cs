0c19389add841c19d11ca601a99b1ab85543e087
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2024-05-26 22:31:52 +0300

    fix database hopefully

--- Models/Favorite.cs
+++ Models/Favorite.cs
@@ -1,10 +1,25 @@
-﻿namespace proiect.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proiect.Models
 {
     public class Favorite
     {
-        internal object Produs;
+        [Key]
+        public int Id { get; set; }
 
-        public object ProdusId { get; internal set; }
-        public object User { get; internal set; }
+        // Foreign key property for the user
+        [ForeignKey("User")]
+        public string FUserId { get; set; }
+
+        // Navigation property for the user
+        public DefaultUser User { get; set; }
+
+        // Foreign key property for the product
+        [ForeignKey("Produs")]
+        public int ProdusId { get; set; }
+
+        // Navigation property for the product
+        public Produs? Produs { get; set; }
     }
 }
--- Models/Review.cs
+++ Models/Review.cs
@@ -1,8 +1,33 @@
-﻿namespace proiect.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace proiect.Models
 {
     public class Review
     {
-        internal object Produs;
-        internal object RUser;
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("Produs")]
+        public int ProdusId { get; set; }
+        public Produs? Produs { get; set; }
+
+        [ForeignKey("DefaultUser")]
+        public string RUserId { get; set; }
+        public DefaultUser? RUser { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Range(1, 5)]
+        public int Rating { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Review()
+        {
+            CreatedAt = DateTime.UtcNow;
+        }
     }
 }
