dfc7c097220c761fb07353f5966f779351238499
RuxiC <ruxandra.cirstea@gmail.com>
2024-05-31 10:51:14 +0300

    filtrare produse

--- Pages/Telefoane.cshtml.cs
+++ Pages/Telefoane.cshtml.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using proiect.ContextModels;
@@ -18,12 +20,49 @@ namespace proiect.Pages.Telefoane
 
         public IList<Produs> Produs { get; set; }
 
-        public async Task OnGet()
+        [BindProperty(SupportsGet = true)]
+        public string Nume { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public decimal? PretMin { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public decimal? PretMax { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? MemorieMin { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public int? MemorieMax { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string Dimensiune { get; set; }
+
+        public async Task OnGetAsync()
         {
-            Produs = _context.Produs
-                .Include(p => p.Categorie)
-                .Where(p => p.Categorie.Nume == "Telefoane")
-                .ToList();
+            IQueryable<Produs> query = _context.Produs.Include(p => p.Categorie).Where(p => p.Categorie.Nume == "Telefoane");
+
+            if (!string.IsNullOrEmpty(Nume))
+            {
+                query = query.Where(p => p.Nume.Contains(Nume));
+            }
+            if (PretMin.HasValue)
+            {
+                query = query.Where(p => p.Pret >= PretMin);
+            }
+            if (PretMax.HasValue)
+            {
+                query = query.Where(p => p.Pret <= PretMax);
+            }
+            if (MemorieMin.HasValue)
+            {
+                query = query.Where(p => p.Memorie >= MemorieMin);
+            }
+            if (MemorieMax.HasValue)
+            {
+                query = query.Where(p => p.Memorie <= MemorieMax);
+            }
+            if (!string.IsNullOrEmpty(Dimensiune))
+            {
+                query = query.Where(p => p.Dimensiune == Dimensiune);
+            }
+
+            Produs = await query.ToListAsync();
         }
     }
 }
